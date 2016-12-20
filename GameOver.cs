d081b1d970203de61fa67f113332dfaac388f5b2
Ahmet Ozan Sivri <postalar06@gmail.com>
2016-12-20 15:38:33 +0400

    Game over implemented.
    TODO: Fix the null exception problem.

---
+++ GameOver.cs
@@ -0,0 +1,41 @@
+﻿/*
+  This portion of the project is implemented by Ahmet Ozan Sivri.
+  12/20/2016
+*/ 
+
+//TODO: Write Comments
+using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	public PlayerHealth playerHealth;
+	public float restartDelay = 5f;
+
+	Animator animate;
+	float restart;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		animate = GetComponent<Animator> ();
+	}
+		
+	// Update is called once per frame
+	void Update () 
+	{
+		if (playerHealth.currentHealth <= 0) 
+		{
+			animate.SetTrigger ("GameOver");
+
+			restart += Time.deltaTime;
+
+			if (restart >= restartDelay)
+			{
+				//TODO: Change the below with SceneManager.
+				//Seems like LoadLevel does not exist anymore
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+}
